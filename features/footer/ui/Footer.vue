ddcb0b6697f33ab7641814ee54b48716d28b7982
ExLyD1 <sashaahnij@gmail.com>
2025-07-22 19:29:11 +0300

    [upd] change iframe link to google maps

--- features/footer/ui/Footer.vue
+++ features/footer/ui/Footer.vue
@@ -5,7 +5,7 @@
 		<div class="h-[250px] sm:h-[300px] lg:h-[350px]">
 			<!-- Пример iframe‑вставки -->
 			<iframe
-				src="https://maps.google.com/maps?q=%D0%B2%D1%83%D0%BB%D0%B8%D1%86%D1%8F+%D0%A2%D0%B5%D0%BF%D0%BB%D0%BE%D0%B2%D0%BE%D0%B7%D0%BD%D0%B0%2C+1%2C+%D0%9A%D0%B8%D1%97%D0%B2%2C+02000&z=17&output=embed"
+				src="https://maps.google.com/maps?q=50.4253008,30.6304923&z=18&output=embed"
 				class="w-full h-full border-none"
 				loading="lazy"
 			></iframe>
