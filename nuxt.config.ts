64bf55e48523643276a4c2f7c1e5f43196039a19
ExLyD1 <sashaahnij@gmail.com>
2025-07-22 11:49:50 +0300

    [+] added img and config cache & [+] added meta tags and web desc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,35 @@ export default defineNuxtConfig({
 		colorMode: false,
 	},
 
+	app: {
+		head: {
+			title: 'santavto', // заголовок страницы по умолчанию
+			titleTemplate: '%s — SANT auto service', // как будет формироваться title на других страницах
+			htmlAttrs: {
+				lang: 'uk', // язык страницы (напр. 'uk' или 'en')
+			},
+			meta: [
+				{
+					name: 'description',
+					content:
+						'Обслуговування та детейлінг авто від SANT auto service',
+				},
+				{
+					name: 'viewport',
+					content: 'width=device-width, initial-scale=1',
+				},
+				// можете добавить Open Graph, Twitter card и т.п.
+				{ property: 'og:title', content: 'SANT auto service' },
+				{
+					property: 'og:description',
+					content:
+						'Захист, детейлінг, фарбування авто і кузовні роботи',
+				},
+			],
+			link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+		},
+	},
+
 	css: ['~/assets/css/splide-overrides.css'],
 
 	runtimeConfig: {
@@ -26,5 +55,64 @@ export default defineNuxtConfig({
 	devServer: {
 		host: '0.0.0.0', // слухає на всіх інтерфейсах
 		port: 3000, // або будь‑який вільний порт
+	},
+
+	nitro: {
+		routeRules: {
+			// Всё в папке _nuxt (скрипты, стили, чанки)
+			'/_nuxt/**': {
+				headers: {
+					// Кешировать на год и пометить как immutable
+					'cache-control': 'public, max-age=31536000, immutable',
+				},
+			},
+			// Например, favicon
+			'/favicon.ico': {
+				headers: {
+					'cache-control': 'public, max-age=31536000, immutable',
+				},
+			},
+			// Если у вас есть папка с картинками
+			'/icons/**': {
+				headers: {
+					'cache-control': 'public, max-age=604800', // 7 дней
+				},
+			},
+			'/portfolio/**': {
+				headers: {
+					'cache-control': 'public, max-age=604800', // 7 дней
+				},
+			},
+			'/reviews/**': {
+				headers: {
+					'cache-control': 'public, max-age=604800', // 7 дней
+				},
+			},
+			'/service-examples/**': {
+				headers: {
+					'cache-control': 'public, max-age=604800', // 7 дней
+				},
+			},
+			'/services/**': {
+				headers: {
+					'cache-control': 'public, max-age=604800', // 7 дней
+				},
+			},
+			'/entities/service/config/**': {
+				headers: {
+					'cache-control': 'public, max-age=604800', // 7 дней
+				},
+			},
+			'/widgets/service/config/**': {
+				headers: {
+					'cache-control': 'public, max-age=604800', // 7 дней
+				},
+			},
+			'/features/**': {
+				headers: {
+					'cache-control': 'public, max-age=604800', // 7 дней
+				},
+			},
+		},
 	},
 })
